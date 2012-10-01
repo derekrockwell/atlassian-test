8d542871dec47abeaf06cf9f04a242008fc16273
drock <derek.a.rockwell@gmail.com>
2012-10-01 10:29:54 -0500

    test add

---
+++ atlassian-test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AtlassianTest::Application.config.secret_token = '93465ac06d186fd96a18502dfdf00ff594856f9ac6a605bc71e3cc3976a1488d5bd410f8e8cbd5a6fad0ae61e579fbfe961bd4f206dff7d800aef3a5e3f6d30a'
